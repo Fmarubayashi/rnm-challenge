3537da9e36249d927eb7014c749a417611825541
fmarubayashi <fernando.marubayashi1@gmail.com>
2022-11-02 18:54:27 -0300

    adding a back arrow to app header

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,10 +1,15 @@
-import Image from 'next/image'
-import styles from './AppHeader.module.scss';
+import { ArrowLeftOutlined } from '@ant-design/icons';
+import Image from 'next/image';
+import Link from 'next/link';
 import Logo from '../../../public/logo.png';
-
-const AppHeader: React.FC = ({ }) => {
+import styles from './AppHeader.module.scss';
+const AppHeader: React.FC = ({}) => {
     return (
         <header className={styles.header}>
+            <Link type="text" className={styles.backArrow} href={'/'}>
+                <ArrowLeftOutlined />
+            </Link>
+
             <Image src={Logo} alt="Logo" width={300} />
         </header>
     );
