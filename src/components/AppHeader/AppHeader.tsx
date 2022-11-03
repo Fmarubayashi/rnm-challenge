36e1384fc2f6d539d5658c778094cfc6c52f289c
fmarubayashi <fernando.marubayashi1@gmail.com>
2022-11-02 21:58:55 -0300

    removing arrow in home page

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,14 +1,18 @@
 import { ArrowLeftOutlined } from '@ant-design/icons';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import Logo from '../../../public/logo.png';
 import styles from './AppHeader.module.scss';
 const AppHeader: React.FC = ({}) => {
+    const { pathname } = useRouter();
     return (
         <header className={styles.header}>
-            <Link type="text" className={styles.backArrow} href={'/'}>
-                <ArrowLeftOutlined />
-            </Link>
+            {pathname !== '/' && (
+                <Link type="text" className={styles.backArrow} href={'/'}>
+                    <ArrowLeftOutlined />
+                </Link>
+            )}
 
             <Image
                 src={Logo}
